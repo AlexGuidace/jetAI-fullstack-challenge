e8a600749b743bf3b494f20c49752bed1f33dcc5
Alex Guidace <alexguidace@gmail.com>
2024-03-17 13:06:33 -0700

    Create Jet model in Prisma schema, run initial Prisma migration into DB, create singleton instance of Prisma for dev mode

---
+++ src/db.ts
@@ -0,0 +1,17 @@
+// This file creates a singleton instance of Prisma for development mode: [Prisma singleton instance documentation](https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices).
+
+import { PrismaClient } from '@prisma/client';
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
