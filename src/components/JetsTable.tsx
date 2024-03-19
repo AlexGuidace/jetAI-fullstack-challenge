126b3b9630d5101fed1a64ae13917a2becfd0c3e
Alex Guidace <alexguidace@gmail.com>
2024-03-18 20:58:25 -0700

    Refactor table population functionality into tableRows variable and assign checkbox element an id

--- src/components/JetsTable.tsx
+++ src/components/JetsTable.tsx
@@ -3,6 +3,23 @@ import '../app/globals.css';
 
 const JetsTable = async (): Promise<JSX.Element> => {
   const jets = await prisma.jet.findMany();
+
+  const tableRows = jets.map((row) => (
+    <tr key={row.id}>
+      <td>
+        <input
+          id={row.name}
+          type="checkbox"
+          className="w-5 h-5 accent-indigo-600"
+        />
+      </td>
+      <td>{row.name}</td>
+      <td>{row.wingspan}</td>
+      <td>{row.engines}</td>
+      <td>{row.year}</td>
+    </tr>
+  ));
+
   return (
     <table className="table-auto border-2 border-neutral-500 p-px mt-4">
       <thead>
@@ -14,19 +31,7 @@ const JetsTable = async (): Promise<JSX.Element> => {
           <th>Manufacturing Year</th>
         </tr>
       </thead>
-      <tbody>
-        {jets.map((row) => (
-          <tr key={row.id}>
-            <td>
-              <input type="checkbox" className="w-5 h-5 accent-indigo-600" />
-            </td>
-            <td>{row.name}</td>
-            <td>{row.wingspan}</td>
-            <td>{row.engines}</td>
-            <td>{row.year}</td>
-          </tr>
-        ))}
-      </tbody>
+      <tbody>{tableRows}</tbody>
     </table>
   );
 };
