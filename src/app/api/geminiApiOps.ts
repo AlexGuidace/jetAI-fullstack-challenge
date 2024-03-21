ddce02b4815f0ca6d7f9c8777f092a750d38e359
Alex Guidace <alexguidace@gmail.com>
2024-03-21 14:25:09 -0700

    Add functionality for Gemini AI prompting that returns a JS object array of answers to the JetsTable based on user inputs for selected jets and comparison criterion

--- src/app/api/geminiApiOps.ts
+++ src/app/api/geminiApiOps.ts
@@ -13,17 +13,44 @@ export const getInfoFromGemini = async (
     checkedJetsArray.shift();
   }
 
+  // Lowercase the search term to prepare it for prompt.
+  const searchTerm = selectedSearchTerm.toLowerCase();
+
   console.log('From Gemini function:');
   console.log(`CHECKED JETS: ${JSON.stringify(checkedJetsArray)}`);
-  console.log(`SELECTED SEARCH TERM: ${selectedSearchTerm}`);
+  console.log(`SELECTED SEARCH TERM: ${searchTerm}`);
 
-  const prompt =
-    'Answer this question: What is the top/max speed in mach for the Bombadier Global 7500 manufactured in 2018, as provided by the Bombadier website? Give me your reply in this format: Top Speed in Mach: numerical value';
-  console.log(prompt);
+  let prompt = `I'd like an answer to each numbered question below. For each question return your answer in this format: a JavaScript object with three camelCased properties: name: the jet name--without its year--provided in the query, the numerical value that you've provided for ${searchTerm}, and its units. Put each of these objects into an array called ${searchTerm}AnswersArray. \n\n`;
+
+  let jetIteration = 0;
+
+  checkedJetsArray.forEach((jet) => {
+    jetIteration++;
+
+    if (searchTerm === 'top speed') {
+      const topSpeedQuery = `What is the ${searchTerm}/max speed in mach units for the ${jet.name} manufactured in ${jet.year}, as provided by the ${jet.name} website? Round the max speed to the nearest hundredth decimal place.`;
+
+      prompt += `${jetIteration}. ${topSpeedQuery} \n\n`;
+    } else if (searchTerm === 'fuel efficiency') {
+      // TODO: Gemini is giving me wildly different values on each request for "fuel efficieny" based on this prompt.
+      const fuelEfficiencyQuery = `What is the ${searchTerm} in nm/gal units for a ${jet.name} manufactured in ${jet.year}, that has a typical cruise speed of 450 knots and full payload, based on ${jet.name} documentation?`;
+
+      prompt += `${jetIteration}. ${fuelEfficiencyQuery} \n\n`;
+    } else if (searchTerm === 'maximum seats') {
+      // TODO: Gemini is giving me different values (off by one seat) based on this prompt.
+      const maxSeatsQuery = `What are the ${searchTerm} on a ${jet.name} manufactured in ${jet.year}, as provided by the ${jet.name} website?`;
+
+      prompt += `${jetIteration}. ${maxSeatsQuery} \n\n`;
+    }
+  });
+
+  console.log(`COMPLETED PROMPT: \n\n ${prompt}`);
 
   try {
-    const result = await model.generateContent(prompt);
-    console.log(result.response.text());
+    const results = await model.generateContent(prompt);
+    const geminiAnswersArray = results.response.text();
+    console.log(geminiAnswersArray);
+    return geminiAnswersArray;
   } catch (error) {
     console.error(`An error occurred while querying Gemini AI: ${error}`);
   }
--- src/components/JetsTable.tsx
+++ src/components/JetsTable.tsx
@@ -41,7 +41,9 @@ const JetsTable: React.FC<Jets> = ({ jets }) => {
   };
 
   //  Sends checkedJetsArray and selectedSearchTerm to Gemini AI function, so Gemini can fetch information regarding jets the user checked.
-  const handleComparisonFormSubmit = (e: { preventDefault: () => void }) => {
+  const handleComparisonFormSubmit = async (e: {
+    preventDefault: () => void;
+  }) => {
     e.preventDefault();
 
     // Validation for checking if array has only initial state or zero jets in it.
@@ -53,12 +55,16 @@ const JetsTable: React.FC<Jets> = ({ jets }) => {
       return;
     }
 
-    const geminiComparisonArray = getInfoFromGemini(
+    const geminiAnswersArray = await getInfoFromGemini(
       checkedJetsArray,
       selectedSearchTerm
     );
 
-    return geminiComparisonArray;
+    console.log(
+      `ANSWERS ARRAY FROM GEMINI INSIDE JETS TABLE: ${geminiAnswersArray}`
+    );
+
+    return geminiAnswersArray;
   };
 
   const tableRows = jets.map((row) => (
@@ -103,11 +109,11 @@ const JetsTable: React.FC<Jets> = ({ jets }) => {
             className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-gray-900 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
           >
             <option value="Top Speed">Top Speed</option>
-            <option value="Fuel Efficieny">Fuel Efficieny</option>
+            <option value="Fuel Efficiency">Fuel Efficiency</option>
             <option value="Maximum Seats">Maximum Seats</option>
           </select>
           <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-            Ask OpenAI GPT to Compare Selected Jets By
+            Ask Gemini AI to Compare Selected Jets By
           </label>
         </div>
         <button
