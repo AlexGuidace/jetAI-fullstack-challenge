465cc56c499b0590e6227d55bff0d2415d34137e
Alex Guidace <alexguidace@gmail.com>
2024-03-24 11:52:06 -0700

    Edit Gemini AI prompt to return valid JSON (no trailing commas on last property of an object and no trailing commas on the last object in the array), so we can parse the Gemini JSON results correctly

--- src/app/api/geminiApiOps.ts
+++ src/app/api/geminiApiOps.ts
@@ -18,13 +18,13 @@ export const getComparisonDataFromGemini = async (
   const searchTerm = selectedComparisonTerm.toLowerCase();
 
   // Initial Gemini prompt detailing instructions for answering questions pertaining to each jet passed into this function, and how to format answers.
-  let prompt = `I'd like an answer to each numbered question below. For each question return your answer in this format: a JavaScript object with three camelCased properties, each wrapped in a set of double quotes: 
+  let prompt = `I'd like an answer to each numbered question below. For each question return your answer in this format: a JavaScript object with three camelCased properties. Wrap each property in double quotes. Do not include trailing commas on a property that comes last in an object.
   
   - name: the jet name--without its year--provided in the query,
   - an object called jetAttribute, with a property inside it called ${searchTerm}, in camelCase. This property should have a value of the numerical value that you've provided for ${searchTerm},
   - A property called units with the ${searchTerm}'s units as the value of that property. The units value for maximum seats is always seats.
   
-  Put each of these objects into an unlabled array. Give the array back to me in plain text format. \n\n`;
+  Put each of these objects into an unlabled array. Do not include trailing commas on the last object in the array. Give the array back to me in plain text format. \n\n`;
 
   // Concatenate prompts to initial prompt based on jets and the comparison term passed in, e.g., if the search term passed in was 'top speed', we would concatenate the topSpeedQuery string--populated with a jet's properties--to the initial prompt, for each jet in our checkedJetsArray.
   let jetIteration = 0;
@@ -33,7 +33,7 @@ export const getComparisonDataFromGemini = async (
 
     switch (searchTerm) {
       case 'top speed':
-        const topSpeedQuery = `What is the ${searchTerm}/max speed in mach units for the ${jet.name} manufactured in ${jet.year}, as provided by the ${jet.name} website? Round the max speed to the nearest hundredth decimal place.`;
+        const topSpeedQuery = `What is the ${searchTerm}/max speed in Mach units for the ${jet.name} manufactured in ${jet.year}. Provide this data from the ${jet.name} website. Round the max speed to the nearest hundredth decimal place.`;
 
         prompt += `${jetIteration}. ${topSpeedQuery} \n\n`;
         break;
@@ -61,9 +61,11 @@ export const getComparisonDataFromGemini = async (
 
   // With our fully-formed prompt, we make the call to Gemini to generate answers for us.
   try {
-    // TODO: Implement retry logic if this call fails, as it sometimes arbitraily does.
     const results = await model.generateContent(prompt);
     const geminiAnswersText = results.response.text();
+    console.log(
+      `GEMINI TEXT............................: \n\n ${geminiAnswersText}`
+    );
 
     //  Parse the returned string response into an actual array.
     const geminiAnswersArray: GeminiAnswer[] = JSON.parse(geminiAnswersText);
